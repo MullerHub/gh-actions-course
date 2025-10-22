bd41c176d1a7775fbbef0332beb0830d9e2c5923
Murilo Muller (Mac) <murilomuller@protonmail.com>
2025-10-22 10:08:29 -0300

    add dependencias

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,7 +1,11 @@
 const core = require('@actions/core');
- 
-async function run() { 
-  core.info('I am a custom JS action');
+
+async function run() {
+    try {
+        core.info('Starting dependency update check...');
+    } catch (error) {
+        core.setFailed(`Action failed with error: ${error.message}`);
+    }
 }
 
 run();
