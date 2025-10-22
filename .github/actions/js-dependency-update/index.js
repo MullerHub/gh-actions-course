4abd6da60da5af68b2ca4f7227d319f7ee196d73
Murilo Muller (Mac) <murilomuller@protonmail.com>
2025-10-22 10:10:38 -0300

    feat: adiciona @actions/exec e teste simples com pwd

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,8 +1,13 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 
 async function run() {
     try {
         core.info('Starting dependency update check...');
+        
+        // Teste simples do exec
+        core.info('Testing exec functionality...');
+        await exec.exec('pwd');  // Vai mostrar o diretório atual
     } catch (error) {
         core.setFailed(`Action failed with error: ${error.message}`);
     }
